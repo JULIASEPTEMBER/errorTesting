3e03f3f1f52c30a0ff3a52546dc0361d125ff236
Julia <67694189+JULIASEPTEMBER@users.noreply.github.com>
2020-07-14 15:45:51 +0800

    Update TestErrorStream.h

--- TestErrorStream.h
+++ TestErrorStream.h
@@ -4,7 +4,7 @@
 #pragma once
 
 #ifndef __AFXWIN_H__
-	#error "在包含此文件之前包含“stdafx.h”以生成 PCH 文件"
+#error "在包含此文件之前包含“stdafx.h”以生成 PCH 文件"
 #endif
 
 #include "resource.h"       // 主符号
@@ -20,12 +20,12 @@ public:
 	CTestErrorStreamApp();
 
 
-// 重写
+	// 重写
 public:
 	virtual BOOL InitInstance();
 	virtual int ExitInstance();
 
-// 实现
+	// 实现
 	UINT  m_nAppLook;
 	BOOL  m_bHiColorIcons;
 
