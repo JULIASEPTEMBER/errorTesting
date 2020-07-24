6d918d978f868fb03a2800b288363c737019f8b7
Julia <67694189+JULIASEPTEMBER@users.noreply.github.com>
2020-07-24 11:06:58 +0800

    Update TestErrorStream.h

--- TestErrorStream.h
+++ TestErrorStream.h
@@ -1,44 +1,67 @@
-
-// TestErrorStream.h : TestErrorStream 应用程序的主头文件
-//
 #pragma once
-
-#ifndef __AFXWIN_H__
-#error "在包含此文件之前包含“stdafx.h”以生成 PCH 文件"
-#endif
-
-#include "resource.h"       // 主符号
+#include "afxwin.h"
+#include "afxcmn.h"
 
 
-// CTestErrorStreamApp:
-// 有关此类的实现，请参阅 TestErrorStream.cpp
-//
-
-class CTestErrorStreamApp : public CWinAppEx
+// CDlg_AnalyzeWave 对话框
+struct st_Render
 {
-public:
-	CTestErrorStreamApp();
+	RECT rt;//fill the figure
+	UINT nFrom, nTo;
+	float fXScale, fYScale;
+	float fDisplaceX, fDisplaceY;
+	UINT nMax, nMin, nMaxID, nMinID;
 
-
-	// 重写
-public:
-	virtual BOOL InitInstance();
-	virtual int ExitInstance();
-
-	// 实现
-	UINT  m_nAppLook;
-	BOOL  m_bHiColorIcons;
-
-	virtual void PreLoadState();
-	virtual void LoadCustomState();
-	virtual void SaveCustomState();
-
-	afx_msg void OnAppAbout();
-	DECLARE_MESSAGE_MAP()
-	CWinThread* m_pThread_TCP;
-	void TCPCreatThread(void);			
-	friend UINT TCPThreadProc(LPVOID pm);
-	static int CALLBACK TransformClass(int *pInfo); //par
 };
 
-extern CTestErrorStreamApp theApp;
+class CDlg_AnalyzeWave : public CDialogEx
+{
+	DECLARE_DYNAMIC(CDlg_AnalyzeWave)
+
+public:
+	CDlg_AnalyzeWave(CWnd* pParent = NULL);   // 标准构造函数
+	virtual ~CDlg_AnalyzeWave();
+
+	// 对话框数据
+	enum { IDD = IDD_DIALOG_OUTPUT_WAVE };
+
+protected:
+	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV 支持
+
+	DECLARE_MESSAGE_MAP()
+public:
+	afx_msg void OnBnClickedButtonWantWave();
+	CEdit m_HorDelta;
+	CEdit m_Edit_VerDelta;
+	CEdit m_Edit_XBegin;
+	CEdit m_Edit_YBegin;
+	afx_msg void OnDeltaposSpin1(NMHDR *pNMHDR, LRESULT *pResult);
+	afx_msg void OnDeltaposSpin2(NMHDR *pNMHDR, LRESULT *pResult);
+	afx_msg void OnDeltaposSpin3(NMHDR *pNMHDR, LRESULT *pResult);
+	afx_msg void OnDeltaposSpin4(NMHDR *pNMHDR, LRESULT *pResult);
+	void Create(HWND hd);
+	CSpinButtonCtrl m_SPin_horDel;
+	CSpinButtonCtrl m_SPin_VerDel;
+	CSpinButtonCtrl m_SPin_XBeg;
+	CSpinButtonCtrl m_SPin_YBeg;
+	void InsertDatas(int nHeight);//insert data to the series
+	HWND out_hd;
+	void SketchOutput();//output those buffer through a st_Render filter
+	afx_msg void OnBnClickedButtonOutput();
+	st_Render m_stRender;
+	void GenerateStructRender();//change the struct other variable 
+	CEdit m_Edit_Beg;
+	CEdit m_Edit_End;
+	afx_msg void OnBnClickedButtonClear();
+	CEdit m_Edit_Left;
+	CEdit m_Edit_Right;
+	CEdit m_Edit_Top;
+	CEdit m_Edit_Bottom;
+	CButton m_Check_Coordinate;
+	void SketchCoordinate(CPoint pt);
+	int HeightInLine(CPoint* ptLine, int nHeight, CPoint* pt);//return the correct point while the pt pointer was not zero 
+	void FillBufferInBitmap();//bitmap  output
+	void OutputFigure(CDC* cdc);//output a figure 
+	void OutputFigure_Particular(CDC* cdc, CPoint pt);//output a figure 
+	void OutputFigure_Particular_Original(CPoint pt);//output a figure 
+};
